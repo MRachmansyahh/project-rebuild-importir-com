b94da65a0011e511e97cd7df83380bc8ac867d7c
MRachmansyahh <rachmansyah.poetra@gmail.com>
2023-11-15 09:49:36 +0700

    test deploy

--- service/firebaseApp.js
+++ service/firebaseApp.js
@@ -15,14 +15,14 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDBtxAx2nFO5fFZYQDqhZE_i6sATZnCBaQ",
+  authDomain: "importir-com-5657a.firebaseapp.com",
+  databaseURL: "https://importir-com-5657a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "importir-com-5657a",
+  storageBucket: "importir-com-5657a.appspot.com",
+  messagingSenderId: "186957587992",
+  appId: "1:186957587992:web:0762ad2e62c1ea6cf5c56d",
+  measurementId: "G-WZXRL6JZ9T"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
